deaf20b3529468506daf888d9954a8c32f0f2bfa
Andy Rosen <ajr@corp.mlfs.org>
2026-05-16 12:04:15 -0400

    Bitwarden Accelerator 8.1.0

--- Formula/b/bitwarden-accelerator.rb
+++ Formula/b/bitwarden-accelerator.rb
@@ -6,10 +6,10 @@ class BitwardenAccelerator < Formula
   homepage 'https://github.com/ajrosen/Bitwarden-Accelerator'
   desc 'Alfred Workflow to make using Bitwarden faster and smoother with all applications'
 
-  version '8.0.1'
+  version '8.1.0'
 
   url "#{homepage}/releases/download/bwa-#{version}/Bitwarden.Accelerator.alfredworkflow"
-  sha256 'b4ddddf94e75b25014cb754e8b94108d9d5ca575e9647ca3c4d2d0d20b10f654'
+  sha256 '07f69b620dd55e3fe124a34cad4035a766d0ea5e76d0decdbd06ff74168b53a9'
 
   depends_on 'bitwarden-cli'
   uses_from_macos 'jq', since: :sequoia
